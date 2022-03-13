ba46e2b5e92b61943042c56e8c7ad4167d8e744a
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-13 16:51:20 -0700

    navbar done

---
+++ design/src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import Navbar from "../components/Navbar";
+
+
+
+
+
+
+const Home = () => {
+  return (
+    <div className="container">
+      <Navbar />
+    </div>
+  );
+};
+
+export default Home;
