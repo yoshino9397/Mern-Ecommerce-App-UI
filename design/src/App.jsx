10b1dfb2a0a39ad8014941cae0dc2510c39fe8d9
矢山佳乃 <yayamayoshino@yayamayoshinonoMacBook-Air.local>
2022-03-13 18:38:48 -0700

    Slider

--- design/src/App.jsx
+++ design/src/App.jsx
@@ -1,11 +1,10 @@
 import React from "react";
 import Home from "./pages/Home";
-import Navbar from "./components/Navbar";
 
 function App() {
   return (
     <>
-      <Navbar />
+      <Home />
     </>
   );
 }
--- design/src/pages/Home.jsx
+++ design/src/pages/Home.jsx
@@ -1,15 +1,14 @@
 import React from "react";
 import Navbar from "../components/Navbar";
-
-
-
-
-
+import Announcement from "../components/Announcement";
+import Slider from "../components/Slider";
 
 const Home = () => {
   return (
-    <div className="container">
+    <div>
+      <Announcement />
       <Navbar />
+      <Slider/>
     </div>
   );
 };
